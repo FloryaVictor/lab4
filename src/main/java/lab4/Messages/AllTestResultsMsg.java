5b03fb38fad4767687d646f78116afbceccfc5d9
FloryaVictor <lapunixa@yandex.ru>
2020-11-14 23:15:54 +0300

    Scripted auto-commit on change (2020-11-14 23:15:54) by gitwatch.sh

--- src/main/java/lab4/Messages/AllTestResultsMsg.java
+++ src/main/java/lab4/Messages/AllTestResultsMsg.java
@@ -1,2 +1,13 @@
-package lab4.Messages;public class AllTestResultsMsg {
+package lab4.Messages;
+
+import java.util.ArrayList;
+
+public class AllTestResultsMsg {
+    private final String testId;
+    private final ArrayList<String> results;
+
+    public AllTestResultsMsg(String testId, ArrayList<String> results){
+        this.testId = testId;
+        
+    }
 }
