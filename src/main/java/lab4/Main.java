b161668263b8c475823b7b24c5f50fabd6a8178f
FloryaVictor <lapunixa@yandex.ru>
2020-11-14 21:53:03 +0300

    Scripted auto-commit on change (2020-11-14 21:53:03) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -1,12 +1,14 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args){
-
+        ActorSystem system = ActorSystem.create("lab4");
+        
     }
 }
