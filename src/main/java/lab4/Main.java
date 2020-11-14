2395fc982c709553921c15090b09da21f97a6221
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 01:00:10 +0300

    Scripted auto-commit on change (2020-11-15 01:00:10) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -3,12 +3,20 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args){
-        ActorSystem system = ActorSystem.create("lab4");
-
+//        ActorSystem system = ActorSystem.create("lab4");
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        ArrayList<Integer> b = a;
+        a.add(4);
+        System.out.println(b.size());
     }
 }
