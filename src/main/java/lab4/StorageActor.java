b777b38e9febdd42f7340661aaa7234cc6e71af6
FloryaVictor <lapunixa@yandex.ru>
2020-11-14 22:54:53 +0300

    Scripted auto-commit on change (2020-11-14 22:54:53) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -1,23 +1,24 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
-import java.util.Map;
+import java.util.HashMap;
 import lab4.Messages.*;
 
 public class StorageActor extends AbstractActor {
-    private Map<String, String> testResultById;
+    private final HashMap<String, String> testResultById = new HashMap<>();
 
 
     @Override
     public Receive createReceive() {
-
         return ReceiveBuilder.create()
                 .match(TestResultMsg.class, result -> {
                     testResultById.put(result.getTestId(), result.getTestResult());
                 })
                 .match(GetTestResultMsg.class, req -> {
-
+                    String id = req.getTestId();
+                    sender().tell(new TestResultMsg(id, testResultById.get(id)), ActorRef.noSender());
                 })
                 .build();
     }
