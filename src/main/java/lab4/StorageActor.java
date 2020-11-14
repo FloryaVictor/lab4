dfdbc2390042afc2481e1d391de6344e5411b4fd
FloryaVictor <lapunixa@yandex.ru>
2020-11-14 21:55:44 +0300

    Scripted auto-commit on change (2020-11-14 21:55:44) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -2,7 +2,10 @@ package lab4;
 
 import akka.actor.AbstractActor;
 
+
 public class StorageActor extends AbstractActor {
+    private Map<String, String>
+
     @Override
     public Receive createReceive() {
         return null;
