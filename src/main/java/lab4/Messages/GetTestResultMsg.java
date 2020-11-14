a0b2f9e593ee9be8ddc1a3f052b4242393ba8cd6
FloryaVictor <lapunixa@yandex.ru>
2020-11-14 22:42:13 +0300

    Scripted auto-commit on change (2020-11-14 22:42:13) by gitwatch.sh

--- src/main/java/lab4/Messages/GetTestResultMsg.java
+++ src/main/java/lab4/Messages/GetTestResultMsg.java
@@ -1,8 +1,13 @@
 package lab4.Messages;
 
 public class GetTestResultMsg {
-    public String msg;
-    public GetTestResultMsg(){
+    public final String testId;
 
+    public GetTestResultMsg(String testId){
+        this.testId = testId;
+    }
+
+    public String value(){
+        return testId;
     }
 }
--- src/main/java/lab4/Messages/TestResultMsg.java
+++ src/main/java/lab4/Messages/TestResultMsg.java
@@ -1,7 +1,13 @@
 package lab4.Messages;
 
 public class TestResultMsg {
-    public TestResultMsg(){
+    public final String testResult;
 
+    public TestResultMsg(String testResult){
+        this.testResult = testResult;
+    }
+
+    public String value(){
+        return testResult;
     }
 }
