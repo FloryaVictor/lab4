7dbb324c36cf407942ccc48fe4b318b1f42d2ba6
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 20:47:00 +0300

    Scripted auto-commit on change (2020-11-15 20:47:00) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -42,20 +42,18 @@ public class Main {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef routerActor = system.actorOf(Props.create(RouterActor.class));
-//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-//                createRoute(system, routerActor).flow(system, materializer);
-//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-//                routeFlow,
-//                ConnectHttp.toHost("localhost", 8080),
-//                materializer
-//        );
-//        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-//        System.in.read();
-//        binding
-//                .thenCompose(ServerBinding::unbind)
-//                .thenAccept(unbound -> system.terminate());
-
-
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                createRoute(system, routerActor).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 
     public static Route createRoute(ActorSystem system, ActorRef routerActor) {
