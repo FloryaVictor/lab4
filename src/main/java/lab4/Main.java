09f100b1fbd643b8d9e2e1fd78da76a9ec9653d6
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 15:19:50 +0300

    Scripted auto-commit on change (2020-11-15 15:19:50) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -22,25 +22,23 @@ import java.util.concurrent.CompletionStage;
 
 public class Main {
     public static void main(String[] args){
-//        ActorSystem system = ActorSystem.create("routes");
-//        final Http http = Http.get(system);
-//        final ActorMaterializer materializer = ActorMaterializer.create(system);
-//        MainHttp instance = new MainHttp(system);
-//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-//                instance.createRoute(system).flow(system, materializer);
-//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-//                routeFlow,
-//                ConnectHttp.toHost("localhost", 8080),
-//                materializer
-//        );
-//        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-//        System.in.wait();
-//        binding
-//                .thenCompose(ServerBinding::unbind)
-//                .thenAccept(unbound -> system.terminate());
-
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress ENTER to stop...");
         Scanner scan = new Scanner(System.in);
         scan.nextLine();
-        System.out.println(1);
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
     }
 }
