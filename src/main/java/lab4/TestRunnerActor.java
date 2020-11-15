f6a2872560ee3f8a8ddfc49e5b5484ff83a0f1f7
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 14:23:21 +0300

    Scripted auto-commit on change (2020-11-15 14:23:21) by gitwatch.sh

--- src/main/java/lab4/TestRunnerActor.java
+++ src/main/java/lab4/TestRunnerActor.java
@@ -4,12 +4,18 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 import lab4.Messages.RunTestMsg;
 
+import javax.script.ScriptEngine;
+
 public class TestRunnerActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(RunTestMsg.class, req -> {
-                    
+                    ScriptEngine engine = new
+                            ScriptEngineManager().getEngineByName("nashorn");
+                    engine.eval(jscript);
+                    Invocable invocable = (Invocable) engine;
+                    return invocable.invokeFunction(functionName, params).toString();
                 }).build();
     }
 }
