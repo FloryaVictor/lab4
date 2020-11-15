a5f1a95f25405d3d172d1f18f9dc4bf3638153e0
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 15:00:40 +0300

    Scripted auto-commit on change (2020-11-15 15:00:40) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -33,7 +33,6 @@ public class Main {
                 materializer
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
