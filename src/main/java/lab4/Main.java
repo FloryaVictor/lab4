df0f37494b97f4178e5749325b2ef4c23d73f606
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 20:52:51 +0300

    Scripted auto-commit on change (2020-11-15 20:52:51) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -60,7 +60,6 @@ public class Main {
         return concat(
                 get(()->
                         parameter("packageId",(id)-> {
-                            System.out.println(1);
                             Future<String> f = ask(routerActor, new GetTestResultsMsg(id), timeout)
                                     .map(Object::toString, system.getDispatcher());
                             return completeOKWithFutureString(f);
