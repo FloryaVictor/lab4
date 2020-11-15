e618eb904447084f2cac9e46561792999466487d
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 15:28:02 +0300

    Scripted auto-commit on change (2020-11-15 15:28:02) by gitwatch.sh

--- src/main/java/lab4/MainHttp.java
+++ src/main/java/lab4/MainHttp.java
@@ -3,14 +3,13 @@ package lab4;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.AllDirectives.*;
 import akka.http.javadsl.server.Route;
 
 import java.util.concurrent.CompletionStage;
 
 public class MainHttp {
-    public Route createRoute(ActorSystem system)
-    {
-        p
+    public Route createRoute(ActorSystem system) {
+        
     }
 }
