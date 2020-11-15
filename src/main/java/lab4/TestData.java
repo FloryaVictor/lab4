02c4c446d8a234c919536d465b0633159d9abb96
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 18:01:42 +0300

    Scripted auto-commit on change (2020-11-15 18:01:42) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -1,5 +1,7 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class TestData {
     private final String id;
     private final String code;
@@ -7,5 +9,43 @@ public class TestData {
     private final String testName;
     private final String expectedValue;
     private final String params;
-    public TestData()
+    public TestData(String id, String code, String funcName, String testName, String expectedValue, String params){
+        this.id = id;
+        this.code = code;
+        this.funcName = funcName;
+        this.testName = testName;
+        this.expectedValue = expectedValue;
+        this.params = params;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getFuncName() {
+        return funcName;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedValue() {
+        return expectedValue;
+    }
+
+    public String getParams() {
+        return params;
+    }
+
+    public static ArrayList<TestData> fromJSON(String json){
+        ArrayList<TestData> parsedTests = new ArrayList<>();
+        
+
+        return parsedTests;
+    }
 }
