229e350d099aeeec64c440701ee57e90018f5ea7
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 18:33:54 +0300

    Scripted auto-commit on change (2020-11-15 18:33:54) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -49,15 +49,19 @@ public class TestData {
         String id = data.getString("packageId");
         String code = data.getString("jsScript");
         String funcName = data.getString("functionName");
-        
-        String[] tests = CDL.toString(data.getJSONArray("tests")).split(",");
-        for(String test : tests){
-            JSONObject testData = new JSONObject(test);
-            parsedTests.add(new TestData(id, code, funcName,
-                                        testData.getString("testName"),
-                                        testData.getString("expectedValue"),
-                                        testData.getString("params")));
-        }
+
+
+        System.out.println(data.getJSONArray("test").get(0).toString());
+
+
+//        String[] tests = CDL.toString(data.getJSONArray("tests")).split(",");
+//        for(String test : tests){
+//            JSONObject testData = new JSONObject(test);
+//            parsedTests.add(new TestData(id, code, funcName,
+//                                        testData.getString("testName"),
+//                                        testData.getString("expectedValue"),
+//                                        testData.getString("params")));
+//        }
         return parsedTests;
     }
 }
