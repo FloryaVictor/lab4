e445a7745c6d1f00c2c95349f73c5123c506f8b0
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 14:21:57 +0300

    Scripted auto-commit on change (2020-11-15 14:21:57) by gitwatch.sh

--- src/main/java/lab4/TestRunnerActor.java
+++ src/main/java/lab4/TestRunnerActor.java
@@ -1,5 +1,10 @@
 package lab4;
 
-public class TestRunnerActor {
+import akka.actor.AbstractActor;
 
+public class TestRunnerActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
