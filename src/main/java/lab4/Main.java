7b7bf625d7d5ff0c5e977455aaa1dc2fc72e7f56
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 22:37:34 +0300

    Scripted auto-commit on change (2020-11-15 22:37:34) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -60,6 +60,7 @@ public class Main {
                         })),
                 post(()->
                         extractDataBytes(data -> {
+                            data.runFold("", )
                             System.out.println(data.toString());
                             ArrayList<TestData> testData = TestData.fromJSON(data.toString());
                             for(TestData t : testData){
