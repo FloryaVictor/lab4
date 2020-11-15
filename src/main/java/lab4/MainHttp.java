8884be9f406e4054d1bfd02d5314083b310975ed
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 19:30:11 +0300

    Scripted auto-commit on change (2020-11-15 19:30:11) by gitwatch.sh

--- src/main/java/lab4/MainHttp.java
+++ src/main/java/lab4/MainHttp.java
@@ -3,32 +3,25 @@ package lab4;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.dispatch.OnComplete;
-import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.server.Route;
 import akka.routing.RouterActor;
 import akka.util.Timeout;
 import lab4.Messages.GetTestResultsMsg;
-import lab4.Messages.SomeTestResultsMsg;
 import scala.concurrent.Future;
 
 
 import java.time.Duration;
-import java.util.concurrent.CompletionStage;
+
 
 import static akka.http.javadsl.server.Directives.*;
 import static akka.pattern.Patterns.ask;
 
-
 public class MainHttp {
-    private final ActorSystem system;
     private final ActorRef routerActor;
     private final Timeout timeout = Timeout.create(Duration.ofSeconds(5));
 
     public MainHttp(ActorSystem system)
     {
-        this.system = system;
         this.routerActor = system.actorOf(Props.create(RouterActor.class));
     }
 
