2df894dec47d8082e6522e3fc6f4679be4914a4a
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 17:54:47 +0300

    Scripted auto-commit on change (2020-11-15 17:54:47) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -4,6 +4,7 @@ public class TestData {
     private final String id;
     private final String code;
     private final String funcName;
+    private final String testName;
     private final String 
     public TestData()
 }
