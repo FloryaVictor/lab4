9dae714b89753e868b030b47d61506a13c868b48
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 20:10:34 +0300

    Scripted auto-commit on change (2020-11-15 20:10:34) by gitwatch.sh

--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -4,6 +4,7 @@ import akka.NotUsed;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -12,43 +13,48 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
 
 
-import akka.routing.RoutedActorRef;
+import akka.routing.RouterActor;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import akka.util.Timeout;
 import lab4.Messages.GetTestResultsMsg;
 import lab4.Messages.RunTestMsg;
 import scala.concurrent.Future;
 
 
+import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
 import static akka.pattern.Patterns.ask;
 
 public class Main {
-    public static void main(String[] args) {
+     final Timeout timeout = Timeout.create(Duration.ofSeconds(5));
+
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
-        MainHttp instance = new MainHttp(system);
+        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class));
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createRoute(system).flow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                createRoute(system, routerActor).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
         System.out.println("Server online at http://localhost:8080/\nPress ENTER to stop...");
-        Scanner scan = new Scanner(System.in);
-        scan.nextLine();
+        System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
 
 
     }
-    public static Route createRoute(ActorSystem system) {
+
+    public static Route createRoute(ActorSystem system, ActorRef routerActor) {
         return route(
                 get(()->
                         parameter("packageId",(id)-> {
