cecea4a97b03b1478f2635a23cf9c9b76cd1d0ac
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 16:40:09 +0300

    Scripted auto-commit on change (2020-11-15 16:40:09) by gitwatch.sh

--- src/main/java/lab4/Actors/RouterActor.java
+++ src/main/java/lab4/Actors/RouterActor.java
@@ -37,7 +37,7 @@ public class RouterActor extends AbstractActor {
         return ReceiveBuilder.create()
                 .match(RunTestMsg.class, test->{
                     Future<Object> f = ask(testRunnersPool, test, timeout);
-
+                    
                 })
                 .match(GetTestResultsMsg.class, req -> {
 
