e53b6e9887ac76a49652f1cc684c4d091048773f
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 18:15:34 +0300

    Scripted auto-commit on change (2020-11-15 18:15:34) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -1,7 +1,7 @@
 package lab4;
 
 import java.util.ArrayList;
-
+import org.json.*;
 
 public class TestData {
     private final String id;
@@ -45,7 +45,7 @@ public class TestData {
 
     public static ArrayList<TestData> fromJSON(String json){
         ArrayList<TestData> parsedTests = new ArrayList<>();
-        JSONParser
+        
 
         return parsedTests;
     }
