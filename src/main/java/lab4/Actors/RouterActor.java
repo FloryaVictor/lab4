3c440a5c71b6c4db7d16b3954a9a18afef2251e5
FloryaVictor <lapunixa@yandex.ru>
2020-11-15 15:56:59 +0300

    Scripted auto-commit on change (2020-11-15 15:56:59) by gitwatch.sh

--- src/main/java/lab4/Actors/RouterActor.java
+++ src/main/java/lab4/Actors/RouterActor.java
@@ -3,6 +3,9 @@ package lab4.Actors;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
+import lab4.Actors.*;
+import lab4.Messages.*;
+
 public class RouterActor extends AbstractActor {
     private final ActorRef testActorsPool = getContext().actorOf(
             
