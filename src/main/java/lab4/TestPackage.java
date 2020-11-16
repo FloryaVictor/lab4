47b168035080c6c516230850bed1edf9ce505b94
FloryaVictor <lapunixa@yandex.ru>
2020-11-16 19:34:44 +0300

    Scripted auto-commit on change (2020-11-16 19:34:44) by gitwatch.sh

--- src/main/java/lab4/TestPackage.java
+++ src/main/java/lab4/TestPackage.java
@@ -1,15 +1,17 @@
 package lab4;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public class TestPackage {
-    @JsonRawValue
+    @JsonValue
     public String json;
-    
+
 
     @JsonCreator
-    public TestPackage(){
+    public TestPackage(@JsonProperty()){
 
     }
 }
