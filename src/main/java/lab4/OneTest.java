18e074902289d3a04a58d85b8de6c3d7fdd89f1a
FloryaVictor <lapunixa@yandex.ru>
2020-11-16 19:19:03 +0300

    Scripted auto-commit on change (2020-11-16 19:19:03) by gitwatch.sh

--- src/main/java/lab4/OneTest.java
+++ src/main/java/lab4/OneTest.java
@@ -1,5 +1,24 @@
 package lab4;
 
 public class OneTest {
+    private final String testName;
+    private final String expectedResult;
+    private final String params;
+    public OneTest(String testName, String expectedResult, String params) {
+        this.testName= testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public String getParams() {
+        return params;
+    }
 }
--- src/main/java/lab4/TestPackage.java
+++ src/main/java/lab4/TestPackage.java
@@ -1,8 +1,15 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class TestPackage {
     private final String id;
     private final String code;
     private final String funcName;
-
+    private final ArrayList<OneTest> tests;
+    public  TestPackage(String id, String code, String funcName){
+        this.id = id;
+        this.code = code;
+        this.funcName = funcName;
+    }
 }
